1018c1b279041423a24ef57303b6896823cb69a7
Leonardo Luz <leonardoluz10@hotmail.com>
2025-02-22 20:51:16 -0300

    feat: :white_check_mark: Feat: Componente Anuncie e Botao adicionado, refatorado manifets e componentes Carrinho e PaginaPadrão.

--- src/pages/Anuncie/index.js
+++ src/pages/Anuncie/index.js
@@ -1,4 +1,56 @@
+import Header from "components/Header";
 import styles from "./Anuncie.module.scss";
+import { useSelector } from "react-redux";
+import Botao from "components/Botao";
+import { useForm } from "react-hook-form";
 export default function Anuncie() {
-  return <div className={styles}>Anuncie</div>;
+  const { register, handleSubmit } = useForm();
+  const categorias = useSelector((state) =>
+    state.categorias.map(({ id, nome }) => ({ id, nome }))
+  );
+  const cadastrar = (valores) => {
+    console.log(valores);
+  };
+
+  return (
+    <div className={styles.container}>
+      <Header
+        titulo="Anuncie aqui"
+        descricao="Anuncie seus produtos no melhor site do Brasil"
+      />
+      <form className={styles.formulario} onSubmit={handleSubmit(cadastrar)}>
+        <input
+          {...register("nome")}
+          placeholder="Nome do produto"
+          alt="Nome do produto"
+        />
+        <input
+          {...register("descricao")}
+          placeholder="Descrição do produto"
+          alt="Descrição do produto"
+        />
+        <input
+          {...register("imagem")}
+          placeholder="URL da imagem do produto"
+          alt="URL da imagem do produto"
+        />
+        <select {...register("categoria")}>
+          <option disabled value="">
+            Selecione uma categoria
+          </option>
+          {categorias.map((categoria) => (
+            <option key={categoria.id} value={categoria.id}>
+              {categoria.nome}
+            </option>
+          ))}
+        </select>
+        <input
+          {...register("preco")}
+          type="number"
+          placeholder="Valor do produto"
+        />
+        <Botao type="submit">Cadastrar produto</Botao>
+      </form>
+    </div>
+  );
 }
