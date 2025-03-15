2969da2cb421719864edf5154ade24e7a4d5f51f
Leonardo Luz <leonardoluz10@hotmail.com>
2025-03-14 21:47:51 -0300

    feat: :rocket: Finalizando curso redux-saga. refatorando componente carrinho na pasta sagas para função finalizarCompra, componente pagamento concluído.

--- src/store/reducers/categorias.js
+++ src/store/reducers/categorias.js
@@ -1,9 +1,6 @@
 import { createAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import categoriasService from "services/categorias";
-import { createStandaloneToast } from "@chakra-ui/toast";
-import { resetarCarrinho } from "./carrinho";
 const initialState = [];
-const { toast } = createStandaloneToast();
 
 export const carregarCategorias = createAction("categorias/carregarCategorias");
 export const carregarUmaCategoria = createAction(
@@ -26,17 +23,7 @@ const categoriasSlice = createSlice({
       state.push(payload);
     },
   },
-  extraReducers: (builder) => {
-    builder.addCase(resetarCarrinho.type, () => {
-      toast({
-        title: "Sucesso",
-        description: "Compra finalizada com sucesso",
-        status: "success",
-        duration: 2500,
-        isClosable: false,
-      });
-    });
-  },
 });
-export const { adicionarTodasACategorias, adicionarUmaCategoria } = categoriasSlice.actions;
+export const { adicionarTodasACategorias, adicionarUmaCategoria } =
+  categoriasSlice.actions;
 export default categoriasSlice.reducer;
